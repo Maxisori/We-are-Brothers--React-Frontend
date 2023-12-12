076075cfbf4186422fcbed71ca51893cc7ad8274
Maxisori <Mj.soria@alumno.etec.um.edu.ar>
2023-12-11 23:11:54 -0300

    detalles

--- src/componentes/body/body.jsx
+++ src/componentes/body/body.jsx
@@ -4,12 +4,15 @@ import './body.css';
 export default function Body() {
   return (
     <>
-    <div class="producto">
-      <img className='imagen-producto' src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI5UbDe-md36cDRZkcBELXNjvRBG_uO2T1UQ&usqp=CAU'} alt='Remera Oversize' />
-        <h2 className='nombreproducto' >Remera Oversize</h2>
-        <p className='precio' >Precio: $3000</p>
-        <button class="boton-carrito" onclick="agregarAlCarrito('Remera Oversize', 3000,)">Agregar al Carrito</button>
-    </div>
+    <body>
+      <div class="producto">
+        <img className='imagen-producto' src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI5UbDe-md36cDRZkcBELXNjvRBG_uO2T1UQ&usqp=CAU'} alt='Remera Oversize' />
+          <h2 className='nombreproducto' >Remera Oversize</h2>
+          <p className='precio' >Precio: $3000</p>
+          <button class="boton-carrito" onclick="agregarAlCarrito('Remera Oversize', 3000,)">Agregar al Carrito</button>
+      </div>
+    </body>
+    
   </>
   );
 }
